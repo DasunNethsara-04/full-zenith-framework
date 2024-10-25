d3cac07e290cb3cf4394ec63483328cbbe512218
Dasun <techsaralk.pro@gmail.com>
2024-10-25 22:33:20 +0530

    Bugs fixed in CLI

--- Public/index.php
+++ Public/index.php
@@ -1,4 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php'; // Ensure this is called first
-require_once '../App/routes.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../App/routes.php';
--- Public/router.php
+++ Public/router.php
@@ -1,4 +1,5 @@
 <?php
+// DO NOT DELETE THIS FILE...
 if (php_sapi_name() === 'cli-server') {
     // Serve the requested resource as-is if it exists
     $filePath = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
