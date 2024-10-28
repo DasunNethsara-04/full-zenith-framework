e08272efac81efbbb30211b03775e4a0c7405e04
Dasun <techsaralk.pro@gmail.com>
2024-10-28 12:38:58 +0530

    .env updated for jwt | jwt added

--- Public/router.php
+++ Public/router.php
@@ -1,10 +1,1 @@
-<?php
-// DO NOT DELETE THIS FILE...
-if (php_sapi_name() === 'cli-server') {
-    // Serve the requested resource as-is if it exists
-    $filePath = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    if (file_exists($filePath) && !is_dir($filePath)) {
-        return false;
-    }
-}
-require 'index.php';
+<?php if (php_sapi_name() === 'cli-server') { $filePath = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); if (file_exists($filePath) && !is_dir($filePath)) return false; } require 'index.php';
