9c876caa47ffcd8214ceed34dade9458fc85722e
Dasun <techsaralk.pro@gmail.com>
2024-10-31 22:56:03 +0530

    comments added to Controller.php

--- Core/src/Controller/Controller.php
+++ Core/src/Controller/Controller.php
@@ -4,19 +4,39 @@ namespace ZenithPHP\Core\Controller;
 
 use PDO;
 
+/**
+ * Abstract base Controller class to provide core functionalities for all controllers.
+ * 
+ * @package ZenithPHP\Core\Controller
+ */
 abstract class Controller
 {
+    /**
+     * Database connection instance.
+     * 
+     * @var PDO
+     */
     protected PDO $pdo;
 
+    /**
+     * Initializes the Controller and sets up the PDO database connection.
+     */
     public function __construct()
     {
         $this->pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";", DB_USER, DB_PASS);
     }
 
+    /**
+     * Renders a view file and passes data to it.
+     * 
+     * @param string $filename The name of the view file (without the `.php` extension).
+     * @param array $data An associative array of data to pass to the view.
+     * 
+     * @return void
+     */
     protected function view($filename = '', $data = []): void
     {
-        // Assuming the project root is one level above 'Core/src/Controller'
-        $baseDir = dirname(__DIR__, 3); // This goes 3 levels up to the project root
+        $baseDir = dirname(__DIR__, 3);
         require_once $baseDir . '/View/' . $filename . '.php';
     }
 }
