8f2000b3418de8598359885d2d042ad8e3b69c26
Dasun <techsaralk.pro@gmail.com>
2024-10-31 23:29:35 +0530

    comments added to CORS.php and routes.php

--- Config/CORS.php
+++ Config/CORS.php
@@ -2,8 +2,25 @@
 
 namespace ZenithPHP\Config;
 
+/**
+ * Class CORS
+ *
+ * The CORS class handles Cross-Origin Resource Sharing (CORS) settings for the application.
+ * It provides methods to set the appropriate HTTP headers to control resource sharing across 
+ * different domains and manage preflight requests.
+ *
+ * @package ZenithPHP\Config
+ */
 class CORS
 {
+    /**
+     * Sets the necessary headers for CORS.
+     *
+     * This method configures the allowed origins, HTTP methods, and headers for cross-origin 
+     * requests. It also handles preflight requests by responding to OPTIONS requests.
+     *
+     * Note: The allowed origins can be restricted to specific domains for security purposes.
+     */
     public static function setHeaders()
     {
         // Set allowed origins
